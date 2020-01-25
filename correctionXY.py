9a4b0bccbc9f4e1b5ec09775baa7afb1f86f1b7d
neurosenya <mrsena1206@gmail.com>
2020-01-25 22:07:26 +0200

    ..

--- correctionXY.py
+++ correctionXY.py
@@ -1,6 +1,7 @@
 import os # for work with directories 
 import numpy as np
-from PIL import Image, ImageSequence # generally for image processing
+from PIL import Image, ImageSequence, ImageEnhance # generally for image processing
+import cv2
 
 
 #_______________________________________useful functions_____________________________________________
@@ -13,8 +14,14 @@ def mat2gray(matrix, amin, amax):
 	# set thresholds for minimum intensity value and maximum
 	matrix[matrix <= amin] = 0; matrix[matrix >= amax] = 1 
 	# rescale the values that are not 0 and 1 to 0/1 scale
-	matrix[matrix != 1] /= matrix.max()
+	matrix[matrix != 1] /= amax
+
+
 	return matrix
+
+def imadjust(matrix):
+	matrix = ((matrix - low_min) / (high_in - low_in)) 
+
 
 def pairwise(iterable):
 	''' function for iterating by two elements in a list'''
@@ -49,7 +56,9 @@ for name in Nfiles:
 
 
 # this loop works with a particular .tif file
-for i in count_tifs:
+ind = 0
+for i in count_tifs: 
+	ind +=1 
 	Name = i
 	Name435 = i[:-4] # Name for a CFP 
 	Name505 = i[:-4]
@@ -61,20 +70,27 @@ for i in count_tifs:
 
 	img_tif = Image.open(path_to_tif)
 	# loops over .png imgs in a .tif file: 2 PNG imgs per loop
+	c = 0
 	for frame1, frame2 in pairwise(ImageSequence.Iterator(img_tif)):
+		c +=1
 		#_________________imitation of mat2gray from MATLAB_______________________#
 		frame1 = mat2gray(frame1, amin, amax)  # not reliable!
 		frame2 = mat2gray(frame2, amin, amax)  # not reliable!
+		if ind == 1 and c == 1:
+			cv2.imshow('image', frame1)
+			cv2.waitKey(0)
 		# prior normalization for better correction
-		max1 = np.max(np.max(Img11))
-        max2 = np.max(np.max(Img21))
+		max1 = np.max(np.max(frame1))
+		max2 = np.max(np.max(frame2)) 
 
-        norm1 = frame1 / max1
-        norm2 = frame2 / max2
-       
-        norm1 = norm1 - 0.018; # I don't know why one needs that, but it was in the script
-        norm2 = norm2 - 0.018;
-        
+		norm1 = frame1 / max1
+		norm2 = frame2 / max2
+		
+		norm1 = norm1 - 0.018; # I don't know why one needs that, but it was in the script
+		norm2 = norm2 - 0.018;
+		# enhance the contrast of frames
+		print(np.shape(norm1))
+
 
 
 
