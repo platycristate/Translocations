76b426e5ab410fd3a9c3b7b0aa26d4569ee4fb2b
neurosenya <mrsena1206@gmail.com>
2020-01-25 09:49:41 +0200

    Morning))

--- correctionXY.py
+++ correctionXY.py
@@ -1,16 +1,29 @@
 import os # for work with directories 
 import numpy as np
 from PIL import Image, ImageSequence # generally for image processing
-import cv2 # for goddamn normalizaion to 0,1 scale
 
+
+#_______________________________________useful functions_____________________________________________
+
+def mat2gray(matrix, amin, amax):
+	"""MATLAB equivalent of mat2gray"""
+	matrix = np.asarray(matrix)
+	# converts to float
+	matrix = matrix.astype('float32') 
+	# set thresholds for minimum intensity value and maximum
+	matrix[matrix <= amin] = 0; matrix[matrix >= amax] = 1 
+	# rescale the values that are not 0 and 1 to 0/1 scale
+	matrix[matrix != 1] /= matrix.max()
+	return matrix
 
 def pairwise(iterable):
 	''' function for iterating by two elements in a list'''
 	a = iter(iterable)
 	return zip(a, a)
 
+#________________________________________________________________
 
-#Constants and names 
+# Constants and names 
 Name_dir2 = 'D:\\Lab\\Translocations_HPCA\\Cell1'
 Name_dir_proc = '\\corr'
 Name_seq_t = '\\p'
@@ -49,16 +62,20 @@ for i in count_tifs:
 	img_tif = Image.open(path_to_tif)
 	# loops over .png imgs in a .tif file: 2 PNG imgs per loop
 	for frame1, frame2 in pairwise(ImageSequence.Iterator(img_tif)):
-		# converts to float
-		frame1 = np.asarray(frame1)
-		frame2 = np.asarray(frame2)
-		cv.normalize(frame1,  0, 1, cv.NORM_MINMAX)
-		print(frame1.max(), frame1.min())
-		frame1 = frame1.astype('float32') 
-		frame2 = frame2.astype('float32')
-		# se thresholds for minimum intensity value and maximum
-		frame1[frame1 <= amin] = 0; frame1[frame1 >= amax] = 1 
-		frame2[frame2 <= amin] = 0; fram1[frame2 >= amax] = 1
+		#_________________imitation of mat2gray from MATLAB_______________________#
+		frame1 = mat2gray(frame1, amin, amax)  # not reliable!
+		frame2 = mat2gray(frame2, amin, amax)  # not reliable!
+		# prior normalization for better correction
+		max1 = np.max(np.max(Img11))
+        max2 = np.max(np.max(Img21))
+
+        norm1 = frame1 / max1
+        norm2 = frame2 / max2
+       
+        norm1 = norm1 - 0.018; # I don't know why one needs that, but it was in the script
+        norm2 = norm2 - 0.018;
+        
+
 
 
 
